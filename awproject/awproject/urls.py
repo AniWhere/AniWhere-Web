6026c7faf51131129f3ff52e985f2cfd57de3fc4
soihs821 <hyunsung9999@gmail.com>
2021-08-10 17:35:26 +0900

    front init commit

---
+++ awproject/awproject/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from awapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.main,name="main"),
+    path('board/',views.board,name="board"),
+    path('bookmark/',views.bookmark,name="bookmark"),
+    path('plan/',views.plan,name="plan"),
+    path('recommend/',views.recommend,name="recommend"),
+]

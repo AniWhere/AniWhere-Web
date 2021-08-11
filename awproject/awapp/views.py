f4859fbace82ce7b8f98a6ce23b48757390b33a9
coocoding <cococode12@gmail.com>
2021-08-11 17:16:31 +0900

    board.html & 지도 기능 추가

--- awproject/awapp/views.py
+++ awproject/awapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
 def main(request):
     return render(request,"main.html")
@@ -15,3 +15,60 @@ def plan(request):
 
 def recommend(request):
     return render(request,"recommend.html")
+
+def showattractions(request):
+    with open('static/json/example.json', encoding='utf-8') as json_file:
+        attractions = json.load(json_file)['response']['body']['items']['item']
+
+    attractiondict = []
+    for attraction in attractions:
+            content = {
+                "title": attraction['title'],
+                "mapx": str(attraction['mapx']),
+                "mapy": str(attraction['mapy']),
+                "addr1": str(attraction['addr1']),
+            }
+            if attraction.get('tel'):
+                content['tel'] = str(attraction['tel'])
+            else:
+                content['tel'] = ''
+            attractiondict.append(content)
+    attractionJson = json.dumps(attractiondict, ensure_ascii=False)
+    return render(request, 'board.html', {'attractionJson': attractionJson})
+
+def search(request):
+    with open('static/json/example.json', encoding='utf-8') as json_file:
+        attractions = json.load(json_file)['response']['body']['items']['item']
+    search_keyword= request.GET.get('key')
+    search_city= request.GET.get('city')
+    attractiondict = []
+
+    for attraction in attractions:
+        if attraction.get('keyword')==search_keyword:
+            if ((attraction.get('addr1')).find(search_city)!=-1 or search_city=="지역"):
+                content = {
+                    "title": attraction['title'],
+                    "mapx": str(attraction['mapx']),
+                    "mapy": str(attraction['mapy']),
+                    "addr1": str(attraction['addr1']),
+                }
+                if attraction.get('tel'):
+                    content['tel'] = str(attraction['tel'])
+                else:
+                    content['tel'] = ''
+                attractiondict.append(content)
+        elif search_keyword=='':
+            if ((attraction.get('addr1')).find(search_city)!=-1 or search_city=="지역"):
+                content = {
+                    "title": attraction['title'],
+                    "mapx": str(attraction['mapx']),
+                    "mapy": str(attraction['mapy']),
+                    "addr1": str(attraction['addr1']),
+                }
+                if attraction.get('tel'):
+                    content['tel'] = str(attraction['tel'])
+                else:
+                    content['tel'] = ''
+                attractiondict.append(content)
+    attractionJson = json.dumps(attractiondict, ensure_ascii=False)
+    return render(request, 'board.html', {'attractionJson': attractionJson})
--- awproject/awproject/urls.py
+++ awproject/awproject/urls.py
@@ -5,8 +5,9 @@ from awapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.main,name="main"),
-    path('board/',views.board,name="board"),
+    path('board/',views.showattractions,name="showattractions"),
     path('bookmark/',views.bookmark,name="bookmark"),
     path('plan/',views.plan,name="plan"),
     path('recommend/',views.recommend,name="recommend"),
+    path('search/',views.search,name="search"),
 ]
